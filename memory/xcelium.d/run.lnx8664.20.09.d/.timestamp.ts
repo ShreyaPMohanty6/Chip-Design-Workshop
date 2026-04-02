ef7927d52211f00bd401f252be32a4b80f7fd290
ShreyaPMohanty6 <shreyamohanty.p6@gmail.com>
2026-04-02 13:34:26 +0530

    Removed large log and dump files

--- memory/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1774595245 /home/user015/Desktop/sscd/memory/memory_tb.v
-1774590260 /home/user015/Desktop/sscd/memory/memory.v
--- memory/xcelium.d/run.lnx8664.20.09.d/files.ts
+++
@@ -1,2 +0,0 @@
-1774590260 /home/user015/Desktop/sscd/memory/memory.v
-1774595245 /home/user015/Desktop/sscd/memory/memory_tb.v
